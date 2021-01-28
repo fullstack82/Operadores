4d8cd8d0dd3b39e657779647f29b9779464e55dd
fullstack82 <fullstack8210@gmail.com>
2021-01-28 10:24:58 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$numero1 = 55;
+$numero2 = 77;
+
+// Operadores aritmeticos
+
+ echo 'Suma: ' . ($numero1 + $numero2) . '</br>' ;   // Nos muestra el resultado de la suma 
+
+echo 'Resta: ' . ($numero1 - $numero2) . '</br>' ;  // Nos muestra el resultado de la resta
+
+echo 'Multiplicación: ' . ($numero1 * $numero2) . '</br>' ;  // Nos muestra el resultado de la multiplicación
+
+
